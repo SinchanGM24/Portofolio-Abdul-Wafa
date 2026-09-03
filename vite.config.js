4629413a3cecbd7378b918b431a6d166b5424536
Abdul Wafa <abdulwafa904@gmail.com>
2026-09-03 23:49:56 +0800

    ci: deploy portfolio to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/Portofolio-Abdul-Wafa/",
   plugins: [react()],
   resolve: {
     alias: {
